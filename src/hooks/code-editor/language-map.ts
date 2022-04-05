f1b5d550835a391ab54c01b48a922d0230046db2
Wilson Ng (Bwayyy) <tosaiya@gmail.com>
2022-04-05 19:57:21 +0800

    Added languages into language map and remove redundant package dependecies

--- src/hooks/code-editor/language-map.ts
+++ src/hooks/code-editor/language-map.ts
@@ -1,4 +1,13 @@
 export const LanguageMap: { [key: string]: string } = {
-  js: "javascript",
-  java: "java",
+  js: "text/javascript",
+  java: "text/x-java",
+  py: "text/x-python",
+  css: "text/css",
+  go: "text/go",
+  sql: "text/sql",
+  vb: "text/vb",
+  c: "text/x-csrc",
+  cs: "text/x-csharp",
+  cpp: "text/x-c++src",
+  rb: "text/x-ruby",
 };
